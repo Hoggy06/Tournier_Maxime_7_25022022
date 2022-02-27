52ea80be3bbce147ae03712b2d22108ce02d9b2e
Max <tourniermaxime@orange.fr>
2022-02-27 13:53:47 +0100

    ajout du middleware auth

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+//Importation de jsonwebtoken
+import jwt from "jsonwebtoken";
+//Exportation du module
+module.exports = (req, res, next) => {
+  try {
+    //Token lié à l'id de l'utilisateur
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+    const userId = decodedToken.userId;
+    req.auth = { userId };
+    //Si incorrect utilisateur invalide
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "Invalid user ID";
+    } else {
+      next();
+    }
+    //Gestion des erreurs
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
