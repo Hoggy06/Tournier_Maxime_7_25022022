3ce3aa32b6303ff9dd8b822f0250b3459869cda0
Max <tourniermaxime@orange.fr>
2022-02-28 18:48:44 +0100

    modif de la config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,37 +1,46 @@
 //Connexion à la bdd
-import { Sequelize, DataTypes } from "sequelize";
-import { usersModel } from "../models/Users.js";
-export function database() {
-  const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.USER,
-    process.env.PASSWORD,
-    {
-      host: process.env.HOST,
-      dialect: "mariadb",
-      port: process.env.PORT,
-      logging: false,
-    }
-  );
+const { Sequelize, DataTypes } = require("sequelize");
+const Comments = require("../models/Comments.js");
+const Likes = require("../models/Likes.js");
+const Posts = require("../models/Posts.js");
+const Roles = require("../models/Roles.js");
+const Users = require("../models/Users.js");
+const dotenv = require("dotenv");
+dotenv.config();
 
-  async function connectToDatabase() {
-    try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mariadb",
+    port: process.env.PORT,
+    logging: false,
   }
-  usersModel();
-  async function sync() {
-    try {
-      await sequelize.sync().then(() => console.log("Sync Succesfull"));
-    } catch (error) {
-      console.log(error);
-    }
+);
+
+const commentsModel = Comments(sequelize, DataTypes);
+const likesModel = Likes(sequelize, DataTypes);
+const postsModel = Posts(sequelize, DataTypes);
+const rolesModel = Roles(sequelize, DataTypes);
+const usersModel = Users(sequelize, DataTypes);
+
+function connectToDatabase(err) {
+  if (err) {
+    console.error(err);
+  } else {
+    sequelize.authenticate();
+    console.log("Connection to database has been established successfully.");
   }
-
-  sync();
-
-  connectToDatabase();
 }
+
+function sync(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    sequelize.sync().then(() => console.log("Synchronization was successful"));
+  }
+}
+
+module.exports = { connectToDatabase, sync };
