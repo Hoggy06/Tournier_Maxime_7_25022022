2bf9da2de59ff2ccd2b48619a4f848d2b2d316d0
Max <tourniermaxime@orange.fr>
2022-03-08 14:27:09 +0100

    maj

--- backend/middlewares/crypto.js
+++ backend/middlewares/crypto.js
@@ -9,17 +9,17 @@ const EMAIL_CRYPTOJS_KEY = process.env.EMAIL_CRYPTOJS_KEY;
 
 // ---------------------- Crypto functions -------------------------
 
-const encryptEmail = (email, key) => {
-  return cryptojs.AES.encrypt(email, key).toString();
+const encryptEmail = (email) => {
+  return cryptojs.AES.encrypt(email, EMAIL_CRYPTOJS_KEY).toString();
 };
 
-const decryptEmail = (encryptEmail, key) => {
-  const decrypted = cryptojs.AES.decrypt(encryptEmail, key);
-  const emailDecrypted = decrypted.toString(cryptojs.enc.Utf8);
-  return emailDecrypted;
+const decryptEmail = (encryptEmail) => {
+  return cryptojs.AES.decrypt(encryptEmail, EMAIL_CRYPTOJS_KEY).toString(
+    cryptojs.enc.Utf8
+  );
 };
 
 // ============================================================
 // ------------------------- EXPORT ---------------------------
 
-module.exports = { encryptEmail, decryptEmail, EMAIL_CRYPTOJS_KEY };
+module.exports = { encryptEmail, decryptEmail };
