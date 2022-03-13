122e4db3fb852b2d3ea934390b519a53b03ad06e
Max <tourniermaxime@orange.fr>
2022-03-13 19:23:21 +0100

    ajout des likes

---
+++ backend/controllers/likes.js
@@ -0,0 +1,79 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database.js");
+const Posts = require("../models/Posts.js")(sequelize, DataTypes);
+const Likes = require("../models/Likes.js")(sequelize, DataTypes);
+
+exports.createLike = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const postId = req.params.postId;
+
+    const post = await Posts.findByPk(postId);
+    if (!post) throw { message: "Post non trouvé" };
+
+    const alreadyLiked = await Likes.findOne({
+      where: { userId, postId },
+    });
+
+    if (alreadyLiked) {
+      if (!alreadyLiked.isLike) {
+        await alreadyLiked.update({ isLike: true });
+        return res.status(201).json({ like: "Like ajouté" });
+      }
+
+      await alreadyLiked.destroy();
+      res.status(201).send({ like: "Like enlevé" });
+    } else {
+      await Likes.create({ postId, userId, isLike: true });
+      res.status(200).send({ like: "Like ajouté" });
+    }
+  } catch (error) {
+    return res.status(404).json(error);
+  }
+};
+
+//Récupération de tous les commentaires d'un membres
+exports.getAllLike = (req, res, next) => {
+  //Jointure de Comments et Users
+  Likes.belongsTo(Posts);
+  Posts.hasMany(Likes);
+  //Data qui seront retournées
+  const options = {
+    limit: 10,
+    order: [["id", "DESC"]],
+    include: {
+      model: Posts,
+      attributes: ["id", "userId"],
+    },
+  };
+
+  Likes.findAll(options)
+    .then((likes) => {
+      res.status(200).json(likes);
+    })
+    .catch(() => {
+      res.status(404).json({ error: "Impossible de retrouver les likes" });
+    });
+};
+
+exports.getOneLike = (req, res, next) => {
+  //Jointure de Comments et Users
+  Likes.belongsTo(Posts);
+  Posts.hasMany(Likes);
+  //Data qui seront retournées
+  const options = {
+    where: { id: req.params.likeId },
+    include: {
+      model: Posts,
+    },
+  };
+  Likes.findOne(options)
+    .then((like) => {
+      res.status(200).json(like);
+    })
+    .catch(() => {
+      res.status(404).json({ error: "Like non trouvé" });
+    });
+};
