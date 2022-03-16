7da7a9f806b4472e866b77142e3eb05444eb2608
Max <tourniermaxime@orange.fr>
2022-03-16 15:33:11 +0100

    Modification de crypto js en Hmac

--- backend/middlewares/crypto.js
+++ backend/middlewares/crypto.js
@@ -10,16 +10,10 @@ const EMAIL_CRYPTOJS_KEY = process.env.EMAIL_CRYPTOJS_KEY;
 // ---------------------- Crypto functions -------------------------
 //Pas de toString ?
 const encryptEmail = (email) => {
-  return cryptojs.AES.encrypt(email, EMAIL_CRYPTOJS_KEY).toString();
-};
-//variables toString ?
-const decryptEmail = (encryptEmail) => {
-  return cryptojs.AES.decrypt(encryptEmail, EMAIL_CRYPTOJS_KEY).toString(
-    cryptojs.enc.Utf8
-  );
+  return cryptojs.HmacSHA256(email, EMAIL_CRYPTOJS_KEY).toString();
 };
 
 // ============================================================
 // ------------------------- EXPORT ---------------------------
 
-module.exports = { encryptEmail, decryptEmail };
+module.exports = { encryptEmail };
