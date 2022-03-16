885accf18e49ac71bfff503d94fa1470de130441
Max <tourniermaxime@orange.fr>
2022-03-16 15:34:58 +0100

    suppression de logout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,5 @@ const multer = require("../middlewares/multer-config.js");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
-router.get("/logout", userCtrl.logout);
 
 module.exports = router;
