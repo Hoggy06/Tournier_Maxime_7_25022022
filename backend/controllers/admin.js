7d06eaf659ca546f22da6691909950ee4345029a
Max <tourniermaxime@orange.fr>
2022-03-16 15:29:50 +0100

    Role ajouté dans le token

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -3,52 +3,29 @@ dotenv.config();
 const { Sequelize, DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database.js");
 const Users = require("../models/Users.js")(sequelize, DataTypes);
-const Roles = require("../models/Roles.js")(sequelize, DataTypes);
 
 exports.setAdmin = (req, res) => {
   Users.findOne({
     where: { id: req.params.id },
   })
     .then((user) => {
-      if (user.id == req.auth.userId) {
-        //console.log(req.auth.userId);
-        return res.status(400).json({ message: "Admin déjà ajouté" });
+      if (req.auth.userId && req.auth.isAdmin === true) {
+        if (user.isAdmin === false) {
+          Users.update({ isAdmin: true }, { where: { id: req.params.id } });
+          res
+            .status(200)
+            .json({ message: "Cet utilisateur est maintenant administrateur" });
+        } else {
+          Users.update({ isAdmin: false }, { where: { id: req.params.id } });
+          res
+            .status(200)
+            .json({ message: "Cet utilisateur n'est plus administrateur" });
+        }
       } else {
-        Roles.create({ userId: req.params.id, isAdmin: true })
-          //On joint les datas de Users
-          .then(() => {
-            res.status(200).json({ message: "Admin ajouté" });
-          })
-          .catch((error) => {
-            res.status(400).json({ error });
-          });
+        return res.status(403).json({ error: "Accès non autorisé" });
       }
     })
     .catch((error) => {
       res.status(404).json({ error });
     });
-
-  /*try {
-    const userId = req.params.id;
-
-    const users = await Users.findByPk(userId);
-    if (!users) throw { message: "Utilisateur non trouvé" };
-
-    const alreadyAdmin = await Roles.findOne({
-      where: { userId },
-    });
-
-    if (alreadyAdmin) {
-      await alreadyAdmin.destroy();
-      res.status(201).send({ admin: "Admin enlevé" });
-      if (!alreadyAdmin) {
-        await Roles.create({ userId, isAdmin: true });
-        res.status(200).send({ admin: "Admin ajouté" });
-      }
-    } else {
-      return res.status(403).send({ admin: "Accès non autorisé" });
-    }
-  } catch (error) {
-    return res.status(404).json(error);
-  }*/
 };
