9808bb2e567bee396c2ba4b069097c7d80a6a22e
Max <tourniermaxime@orange.fr>
2022-03-26 17:42:35 +0100

    Page de maj d'un commentaire

---
+++ frontend/src/pages/UpdateOneComment.jsx
@@ -0,0 +1,33 @@
+import "../styles/App.css";
+import Nav from "../components/Base/Nav";
+import Footer from "../components/Base/Footer";
+import { Fragment } from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import UpdateComment from "../components/Comments/UpdateComment";
+import { useParams } from "react-router-dom";
+import { Columns } from "react-bulma-components";
+
+export default function OneComment() {
+  const { id, idPost } = useParams();
+  return (
+    <Fragment>
+      <HelmetProvider>
+        <Helmet>
+          <title>
+            Edition du Commentaire #{id} du Post #{idPost}
+          </title>
+        </Helmet>
+      </HelmetProvider>
+      <Nav />
+      <Columns.Column
+        mobile={{ size: 12 }}
+        tablet={{ size: 12 }}
+        desktop={{ size: 6, offset: 3 }}
+      >
+        <UpdateComment />
+      </Columns.Column>
+
+      <Footer />
+    </Fragment>
+  );
+}
