56f4b89cb16973cada903a623dd3a620571f724f
Max <tourniermaxime@orange.fr>
2022-03-26 17:42:42 +0100

    Page de maj d'un post

---
+++ frontend/src/pages/UpdateOnePost.jsx
@@ -0,0 +1,31 @@
+import "../styles/App.css";
+import Nav from "../components/Base/Nav";
+import Footer from "../components/Base/Footer";
+import { Fragment } from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import UpdatePost from "../components/Post/UpdatePost";
+import { useParams } from "react-router-dom";
+import { Columns } from "react-bulma-components";
+
+export default function OnePost() {
+  const { id } = useParams();
+  return (
+    <Fragment>
+      <HelmetProvider>
+        <Helmet>
+          <title>Edition du Post #{id}</title>
+        </Helmet>
+      </HelmetProvider>
+      <Nav />
+      <Columns.Column
+        mobile={{ size: 12 }}
+        tablet={{ size: 12 }}
+        desktop={{ size: 6, offset: 3 }}
+      >
+        <UpdatePost />
+      </Columns.Column>
+
+      <Footer />
+    </Fragment>
+  );
+}
