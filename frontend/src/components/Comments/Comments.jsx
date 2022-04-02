e25a936c5206bd2db9e65cc7ca762cef527990de
Max <tourniermaxime@orange.fr>
2022-04-02 10:51:38 +0200

    Restructuration des commentaires POST + DELETE + GET

--- frontend/src/components/Comments/Comments.jsx
+++ frontend/src/components/Comments/Comments.jsx
@@ -1,19 +1,59 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import {
+  Form,
+  Button,
+  Media,
+  Box,
+  Image,
+  Content,
+} from "react-bulma-components";
 import moment from "moment";
-import DeleteComment from "./DeleteComment";
-import { Media, Image, Box, Content } from "react-bulma-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { port } from "../../port";
 
 export default function Comments() {
   const userConnected = JSON.parse(localStorage.getItem("userConnected"));
   const token = `Bearer ${userConnected.token}`;
-  const [data, setData] = useState([]);
+  const [message, setMessage] = useState("");
+  const onMessageChange = (e) => setMessage(e.target.value);
   const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [deleteComment, setdeleteComment] = useState(false);
+  moment.locale("fr");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { message };
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(`http://localhost:${port}/api/posts/${id}/comment`, options)
+      .then((response) => response.json())
+      .then(() => {
+        fetch(`http://localhost:${port}/api/posts/${id}/comments`, {
+          method: "GET",
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => setData(data))
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
+    setMessage("");
+  };
 
   useEffect(() => {
-    fetch(`http://localhost:3307/api/posts/${id}/comments`, {
+    fetch(`http://localhost:${port}/api/posts/${id}/comments`, {
       method: "GET",
       headers: {
         Authorization: token,
@@ -23,59 +63,119 @@ export default function Comments() {
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.log(error));
-  }, [id, token, data]);
+  }, [id, token]);
 
-  const render = data.map((i, index) => {
-    return (
-      <Box key={index}>
-        <Media>
-          <Media.Item renderAs="article" align="left">
-            {i.User && (
-              <Link to={`/user/${i.userId}`}>
-                <Image size={64} alt="64x64" src={i.User.image} />
-              </Link>
-            )}
-          </Media.Item>
-          <Media.Item>
-            <Content>
-              <p className="pJustify">
-                {i.User && <b>{i.User.firstname}</b>}{" "}
-                <small>
-                  - Le {moment(i.created).format("DD/MM/YYYY à HH:mm")}
-                </small>
-                <br />
-                {i.message}
-                <br />
-                {i.image ? <Image src={i.image} alt={`${i.image}`} /> : null}
-              </p>
-              <nav className="level is-mobile">
-                <div className="level-left">
-                  {userConnected.userId === i.userId ? (
-                    <Link
-                      className="level-item"
-                      to={`/editComment/${i.id}/post/${id}`}
-                    >
-                      <span
-                        data-tooltip="Editer"
-                        className="icon is-small level-item has-tooltip-bottom"
-                      >
-                        <FontAwesomeIcon icon={faPen} />
-                      </span>
-                    </Link>
-                  ) : null}
-                </div>
-              </nav>
-            </Content>
-          </Media.Item>
-          <Media.Item renderAs="article" align="right">
-            {userConnected.userId === i.userId ||
-            userConnected.isAdmin === true ? (
-              <DeleteComment idComment={i.id} />
-            ) : null}
-          </Media.Item>
-        </Media>
+  return (
+    <Fragment>
+      <Box>
+        <form onSubmit={handleSubmit}>
+          <Media renderAs="article">
+            <Media.Item align="center">
+              <Form.Field>
+                <Form.Control>
+                  <Form.Textarea
+                    className="textarea"
+                    placeholder="Ajouter un commentaire..."
+                    value={message}
+                    onChange={onMessageChange}
+                    name="message"
+                    required
+                    disabled={!userConnected}
+                  ></Form.Textarea>
+                  <Form.Field>
+                    <Form.Control>
+                      <Button color="link" disabled={!userConnected}>
+                        Commenter
+                      </Button>
+                    </Form.Control>
+                  </Form.Field>
+                </Form.Control>
+              </Form.Field>
+            </Media.Item>
+          </Media>
+        </form>
       </Box>
-    );
-  });
-  return render;
+      {data.map((i, index) => {
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          const options = {
+            method: "DELETE",
+            headers: {
+              Authorization: token,
+              "Content-Type": "application/json",
+            },
+          };
+          fetch(`http://localhost:${port}/api/posts/comment/${i.id}`, options)
+            .then((response) => response.json())
+            .then(() => {
+              fetch(`http://localhost:${port}/api/posts/${id}/comments`, {
+                method: "GET",
+                headers: {
+                  Authorization: token,
+                  "Content-Type": "application/json",
+                },
+              })
+                .then((response) => response.json())
+                .then((data) => setData(data))
+                .catch((error) => console.log(error));
+            })
+            .catch((error) => console.log(error));
+          setdeleteComment(!deleteComment);
+        };
+        return (
+          <Box key={index}>
+            <Media>
+              <Media.Item renderAs="article" align="left">
+                {i.User && (
+                  <Link to={`/user/${i.userId}`}>
+                    <Image size={64} alt="64x64" src={i.User.image} />
+                  </Link>
+                )}
+              </Media.Item>
+              <Media.Item>
+                <Content>
+                  <p className="pJustify">
+                    {i.User && <b>{i.User.firstname}</b>}{" "}
+                    <small>
+                      - {moment(i.created).startOf("YYYYMMDD").fromNow()}
+                    </small>
+                    <br />
+                    {i.message}
+                  </p>
+                  <nav className="level is-mobile">
+                    <div className="level-left">
+                      {userConnected.userId === i.userId ? (
+                        <Link
+                          className="level-item"
+                          to={`/editComment/${i.id}/post/${id}`}
+                        >
+                          <span
+                            data-tooltip="Editer"
+                            className="icon is-small level-item has-tooltip-bottom"
+                          >
+                            <FontAwesomeIcon icon={faPen} />
+                          </span>
+                        </Link>
+                      ) : null}
+                    </div>
+                  </nav>
+                </Content>
+              </Media.Item>
+              <Media.Item renderAs="article" align="right">
+                {userConnected.userId === i.userId ||
+                userConnected.isAdmin === true ? (
+                  <span
+                    data-tooltip="Supprimer"
+                    className="icon is-small level-item has-tooltip-bottom"
+                  >
+                    <Button onClick={handleSubmit} className="delete"></Button>
+                  </span>
+                ) : null}
+              </Media.Item>
+            </Media>
+          </Box>
+        );
+      })}
+    </Fragment>
+  );
 }
