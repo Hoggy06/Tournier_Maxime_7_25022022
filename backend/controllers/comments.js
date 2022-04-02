04b01b7dec70eeff9c995386ed7a113700727a82
Max <tourniermaxime@orange.fr>
2022-04-02 10:46:28 +0200

    Modification d'un condition sur la suppression des coms

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -95,7 +95,7 @@ exports.deleteComment = (req, res, next) => {
   Comments.findOne({ where: { id: req.params.id } })
     .then((comment) => {
       //Si l'utilisateur n'est pas l'auteur du commentaire = 403
-      if (comment.userId !== req.auth.userId && req.auth.isAdmin === false) {
+      if (comment.userId !== req.auth.userId) {
         return res.status(403).json({ error: "Accès non autorisé" });
       } else {
         comment.destroy();
