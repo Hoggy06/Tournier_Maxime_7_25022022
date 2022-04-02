d3cc33a0677f8e1821dee077b2194b91e5c7c25b
Max <tourniermaxime@orange.fr>
2022-04-02 10:50:01 +0200

    Modification du footer avec le composant Hero

--- frontend/src/components/Base/Footer.jsx
+++ frontend/src/components/Base/Footer.jsx
@@ -1,17 +1,19 @@
-import { Container, Content, Footer } from "react-bulma-components";
+import { Container, Content, Hero } from "react-bulma-components";
 export default function FooterSection() {
   const newDate = new Date();
   const currentDate = newDate.getFullYear();
 
   return (
-    <Footer className="footer has-background-black-ter has-text-white">
-      <Container>
-        <Content>
-          <p className="content has-text-centered">
-            Groupomania &copy; {currentDate}
-          </p>
-        </Content>
-      </Container>
-    </Footer>
+    <Hero className="hero" color="text" size="small">
+      <Hero.Body>
+        <Container>
+          <Content>
+            <p className="content has-text-centered">
+              Groupomania &copy; {currentDate}
+            </p>
+          </Content>
+        </Container>
+      </Hero.Body>
+    </Hero>
   );
 }
